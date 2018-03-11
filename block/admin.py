6d87b18f07786503878dc61f94fa83360e27b7df
leon <coollinn@gmail.com>
2018-03-11 23:35:18 +0800

    from Mac-Min

--- block/admin.py
+++ block/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Block
 
-#add the new class
+#add the new classasdf
 class BlockAdmin(admin.ModelAdmin):
     list_display=("name", "desc", "manager_name")
 
