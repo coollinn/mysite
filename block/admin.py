d954ad07132556e1a519047b04dfede0006df208
leon <coollinn@gmail.com>
2018-03-11 23:04:22 +0800

    sd

--- block/admin.py
+++ block/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Block
 
+Class BlockAdmin(admin.ModelAdmin):
+    list_display("name", "desc", "manager_name")
+
 # Register your models here.
-admin.site.register(Block)
+admin.site.register(Block, BlockAdmin)
