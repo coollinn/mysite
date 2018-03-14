768a7ba10b27b052fc9f70f78fa287a5eeb73a8a
leon <coollinn@gmail.com>
2018-03-14 23:23:08 +0800

    from home by Mac-Min

--- article/admin.py
+++ article/admin.py
@@ -3,7 +3,7 @@ from article.models import Article
 
 #add the new classasdf
 class ArticalAdmin(admin.ModelAdmin):
-    list_display=("block", "title", "content", "create_timestamp" ,"status")
+    list_display=("block", "title", "content", "create_timestamp", "last_update_timestamp" ,"status")
 
 # Register your models here.
 admin.site.register(Article, ArticalAdmin)
