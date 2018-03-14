be3ff3580f57827e9276e36a1d54691217c2c5f2
leon <coollinn@gmail.com>
2018-03-14 23:22:27 +0800

    from home by Mac-Min

--- block/admin.py
+++ block/admin.py
@@ -3,7 +3,7 @@ from block.models import Block
 
 #add the new classasdf
 class BlockAdmin(admin.ModelAdmin):
-    list_display=("name", "desc", "manager_name")
+    list_display=("name", "desc", "manager_name", "status")
 
 # Register your models here.
 admin.site.register(Block, BlockAdmin)
