74f63adc50974d5cf5ba038f4a88ac005f59a42a
leon <coollinn@gmail.com>
2018-03-14 22:20:05 +0800

    from home by Mac-Min

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
+
+#add the new classasdf
+class ArticalAdmin(admin.ModelAdmin):
+    list_display=("block", "title", "content")
 
 # Register your models here.
+admin.site.register(Article, ArticalAdmin)
--- article/models.py
+++ article/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Modle):
+    block = models.ForeignKey(Block, verbose_name="BlockID")
+    title = models.CharField("ArticleName", max_length=100)
+    content = models.CharField("ArticleDesc", max_length=10000)
+    status = models.IntegerField("Status", choices=((0, "GOOD"), (-1, "DELETE")))
+    create_timestamp = models.DateTimeField("CreateTime", auto_now_add=True)
+    last_update_timestamp = models.DateTimeField("LastUpdateTime", auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Artical"
+        verbose_name_plural = "Artical"
+        
