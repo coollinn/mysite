3c2a820bfcdeea111746b232303781a272dc03e1
leon <coollinn@gmail.com>
2018-03-14 22:40:51 +0800

    from home by Mac-Min

--- article/models.py
+++ article/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from .models import Block
+#from models import Block
 
 # Create your models here.
 class Article(models.Model):
     block = models.ForeignKey(Block, verbose_name="BlockID")
-    title = models.CharField("ArticleName", max_length=100)
-    content = models.CharField("ArticleDesc", max_length=10000)
+    title = models.CharField("BlockName", max_length=100)
+    content = models.CharField("BlockDesc", max_length=10000)
     status = models.IntegerField("Status", choices=((0, "GOOD"), (-1, "DELETE")))
     create_timestamp = models.DateTimeField("CreateTime", auto_now_add=True)
     last_update_timestamp = models.DateTimeField("LastUpdateTime", auto_now=True)
