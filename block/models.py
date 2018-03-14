06532f71221bfe4d48293fbe60639223c616b6a8
W530 <coollinn@gmail.com>
2018-03-14 17:40:32 +0800

    update from office W530

--- block/models.py
+++ block/models.py
@@ -7,3 +7,4 @@ class Block(models.Model):
     name = models.CharField("Block_name", max_length=100)
     desc = models.CharField("Block_Desc", max_length=100)
     manager_name = models.CharField("Block_Admin", max_length=100)
+    status = models.IntegerField("Status", choices=((0, "GOOD"), (-1, "DELETE")))
