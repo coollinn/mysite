54b7514015b800db3129756a911936ea5b736875
leon <coollinn@gmail.com>
2018-03-18 19:05:09 +0800

    update from MacPro15

--- article/views.py
+++ article/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def artical_list(request, block_id):
+def article_list(request, block_id):
     block_id = int(block_id)
-    return render(request, "artical_list.html")
+    return render(request, "article_list.html")
 
