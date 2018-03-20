8d5a25febe58cfd59f5e9e48ec5c37d66b8c07b4
leon <coollinn@gmail.com>
2018-03-20 23:09:02 +0800

    update from MacPro15

--- article/views.py
+++ article/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from block.models import Block
+from article.models import Article
 
 # Create your views here.
 def article_list(request, block_id):
     block_id = int(block_id)
-    return render(request, "article_list.html")
+    block = Block.objects.get(id=block_id)
+    article_jobs = Article.objects.filter(block=block, status=0).order_by("-id")
+    return render(request, "article_list.html", {"article": article_jobs, "b": block})
 
