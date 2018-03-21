bdd5520908c0cad58f05263c05b5061432999690
W530 <coollinn@gmail.com>
2018-03-21 17:02:18 +0800

    update from office W530

--- article/views.py
+++ article/views.py
@@ -11,5 +11,9 @@ def article_list(request, block_id):
     articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
     return render_to_response("article_list.html", {"articles":articles_objs, "b":block})
 
-def article_create(request):
-    return render(request, "article_create.html")
+def article_create(request, block_id)):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+    articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
+    #return render(request, "article_create.html")
+    return render_to_response("article_create.html", {"articles":articles_objs, "b":block})
