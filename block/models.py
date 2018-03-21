ce97fea70883dd047588de1d7de429a3567867af
W530 <coollinn@gmail.com>
2018-03-21 15:33:08 +0800

    update from office W530

--- block/models.py
+++ block/models.py
@@ -8,3 +8,10 @@ class Block(models.Model):
     desc = models.CharField("Block_Desc", max_length=100)
     manager_name = models.CharField("Block_Admin", max_length=100)
     status = models.IntegerField("Status", choices=((0, "GOOD"), (-1, "DELETE")))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name="Block"
+        verbose_name_plural = "Block"
