a75e4a62f3efec63987d135d249826139ebc0b78
W530 <coollinn@gmail.com>
2018-03-22 11:13:23 +0800

    update from office W530

--- article/views.py
+++ article/views.py
@@ -12,18 +12,13 @@ def article_list(request, block_id):
     return render_to_response("article_list.html", {"articles":articles_objs, "b":block})
 
 def article_create(request, block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
     if request.method == "GET":
-        return render(request, "article_display.html")
+        return render(request, "article_create.html", {"b": block})
     else:
-        title = request.POST["title"]
-        content = request.POST["content"]
-        block_id = int(block_id)
-        block = Block.objects.get(id=block_id)
+        title = request.POST["title"].strip()
+        content = request.POST["content"].strip()
         article = Article(block=block, title=title, content=content, status=0)
         article.save()
         return redirect("/article/list/%s" %block_id)
-
-        #block_id = int(block_id)
-        #block = Block.objects.get(id=block_id)
-        #articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
-        #return render_to_response("article_create.html", {"articles":articles_objs, "b":block})
