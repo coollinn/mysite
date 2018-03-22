48f6b5cd492ced80523bf2e2a2d998afd7da8a52
leon <coollinn@gmail.com>
2018-03-22 22:58:25 +0800

    update from MacPro15

--- article/forms.py
+++ article/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 class ArticleForm(forms.Form):
-    title=forms.CharField(lable="标题", max_length=1000)
-    content=forms.CharField(lable="内容", max_length=10000)
+    title=forms.CharField(label="标题", max_length=1000)
+    content=forms.CharField(label="内容", max_length=10000)
