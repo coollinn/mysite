77e6cc3899a87550e90df4448410a6573ea4ab27
W530 <coollinn@gmail.com>
2018-03-22 11:08:05 +0800

    update from office W530

--- article/views.py
+++ article/views.py
@@ -11,9 +11,19 @@ def article_list(request, block_id):
     articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
     return render_to_response("article_list.html", {"articles":articles_objs, "b":block})
 
-def article_create(request):
-    #block_id = int(block_id)
-    #block = Block.objects.get(id=block_id)
-    #articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
-    return render(request, "article_create.html")
-    #return render_to_response("article_create.html", {"articles":articles_objs, "b":block})
+def article_create(request, block_id):
+    if request.method == "GET":
+        return render(request, "article_display.html")
+    else:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        block_id = int(block_id)
+        block = Block.objects.get(id=block_id)
+        article = Article(block=block, title=title, content=content, status=0)
+        article.save()
+        return redirect("/article/list/%s" %block_id)
+
+        #block_id = int(block_id)
+        #block = Block.objects.get(id=block_id)
+        #articles_objs = Article.objects.filter(block=block, status=0).order_by("-id")
+        #return render_to_response("article_create.html", {"articles":articles_objs, "b":block})
