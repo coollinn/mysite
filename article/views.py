37352e00cb0b974dd5b233382e09f03b6c1983ca
leon <coollinn@gmail.com>
2018-03-22 21:55:37 +0800

    update from MacPro15

--- article/views.py
+++ article/views.py
@@ -14,6 +14,8 @@ def article_list(request, block_id):
 def article_create(request, block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
+    title = request.POST["title"].strip()
+    content = request.POST["content"].strip()
     if request.method == "GET":
         return render(request, "article_create.html", {"b": block})
     else:
@@ -23,8 +25,6 @@ def article_create(request, block_id):
         if len(title) > 1000 or len(content) > 10000:
             return render(request, "article_create.html",
                     {"b":block, "error":"标题和内容不能太长", "title":title, "content":content})
-        title = request.POST["title"].strip()
-        content = request.POST["content"].strip()
         article = Article(block=block, title=title, content=content, status=0)
         article.save()
         return redirect("/article/list/%s" %block_id)
