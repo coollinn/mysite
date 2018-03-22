ef7c4ae16d8017d839bb87e0078941fe8fcb816a
leon <coollinn@gmail.com>
2018-03-22 21:44:03 +0800

    update from MacPro15

--- article/views.py
+++ article/views.py
@@ -18,11 +18,11 @@ def article_create(request, block_id):
         return render(request, "article_create.html", {"b": block})
     else:
         if not title or not content:
-            return render(request, "article_create.html")
-                    {"b":block, "error":"标题和内容都不能为空", "title":title, "content":content}
+            return render(request, "article_create.html",
+                    {"b":block, "error":"标题和内容都不能为空", "title":title, "content":content})
         if len(title) > 1000 or len(content) > 10000:
-            return render(request, "article_create.html")
-                    {"b":block, "error":"标题和内容不能太长", "title":title, "content":content}
+            return render(request, "article_create.html",
+                    {"b":block, "error":"标题和内容不能太长", "title":title, "content":content})
         title = request.POST["title"].strip()
         content = request.POST["content"].strip()
         article = Article(block=block, title=title, content=content, status=0)
