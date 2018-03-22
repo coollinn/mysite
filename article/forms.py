97f51a20238db5d3adf8c374308c657c2085e4bd
leon <coollinn@gmail.com>
2018-03-22 23:04:16 +0800

    update from MacPro15

--- article/forms.py
+++ article/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+
+#简化下面的代码，直接从models中提取来生存校验器
+"""
 class ArticleForm(forms.Form):
     title=forms.CharField(label="标题", max_length=1000)
     content=forms.CharField(label="内容", max_length=10000)
+"""
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
