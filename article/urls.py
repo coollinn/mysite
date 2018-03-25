d64a24ce23175753f957d7e99fc56a970eba43ce
leon <coollinn@gmail.com>
2018-03-25 23:03:40 +0800

    update from MacPro15

--- article/urls.py
+++ article/urls.py
@@ -22,7 +22,7 @@ from article.views import article_create
 urlpatterns = [
     url(r'^list/(?P<block_id>\d+)', article_list),
     url(r'^create/(?P<block_id>\d+)', article_create),
-
+    url(r'^detail/(?P<aid>\d+)')
 ]
 
 
--- article/views.py
+++ article/views.py
@@ -42,3 +42,8 @@ def article_create(request, block_id):
             return redirect("/article/list/%s" %block_id)
         else:
             return render(request, "article_create.html", {"b":block, "form":form})
+
+
+def article_detail(request, aid):
+    article = Article.objects.get(id=aid)
+    return render ("article_detail.html", {"article": article})
