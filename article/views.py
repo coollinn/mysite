a7d8b0c019a2c723f3435240e73571a8322add2a
leon <coollinn@gmail.com>
2018-03-25 23:16:46 +0800

    update from MacPro15

--- article/views.py
+++ article/views.py
@@ -46,5 +46,5 @@ def article_create(request, block_id):
 
 def article_detail(request, aid):
     aid = int(aid)
-    article = Article.objects.get(id=aid)
+    article = Article.objects.filter(id=aid)
     return render ("article_detail.html", {"a": article})
